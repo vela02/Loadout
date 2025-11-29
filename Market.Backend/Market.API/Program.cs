bee4490f79221c41eac3b4f8bdc119838e8ce3a1
Adil Joldic <adil@fit.ba>
2025-11-29 01:23:08 +0100

    CORS pravilo: dozvola angular dev servera localhost 4200

--- Market.Backend/Market.API/Program.cs
+++ Market.Backend/Market.API/Program.cs
@@ -49,6 +49,20 @@ public partial class Program
                 .AddInfrastructure(builder.Configuration, builder.Environment)
                 .AddApplication();
 
+            // CORS policy to allow Angular dev server access
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAngularDev",
+                    policy =>
+                    {
+                        policy
+                            .WithOrigins("http://localhost:4200") // kao string array može i više URL-ova
+                            .AllowAnyHeader()
+                            .AllowAnyMethod()
+                            .AllowCredentials();
+                    });
+            });
+
             var app = builder.Build();
 
             // ---------------------------------------------------------
@@ -65,6 +79,9 @@ public partial class Program
             app.UseMiddleware<RequestResponseLoggingMiddleware>();
 
             app.UseHttpsRedirection();
+            // UseCors ide prije UseAuthorization i UseAuthentification
+            app.UseCors("AllowAngularDev");
+
             app.UseAuthentication();
             app.UseAuthorization();
 
