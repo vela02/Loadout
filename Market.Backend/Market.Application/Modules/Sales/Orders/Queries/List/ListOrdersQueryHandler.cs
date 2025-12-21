260028ea9d29aad99d161516c42a447467491dec
velid <velidmadzak@gmail.com>
2025-12-21 23:03:03 +0100

    Adjusted files to fit our project temporarily

--- Market.Backend/Market.Application/Modules/Sales/Orders/Queries/List/ListOrdersQueryHandler.cs
+++ Market.Backend/Market.Application/Modules/Sales/Orders/Queries/List/ListOrdersQueryHandler.cs
@@ -6,38 +6,49 @@ public sealed class ListOrdersQueryHandler(IAppDbContext ctx, IAppCurrentUser cu
 
     public async Task<PageResult<ListOrdersQueryDto>> Handle(ListOrdersQuery request, CancellationToken ct)
     {
-        var q = ctx.Orders.AsNoTracking();
+        //var q = ctx.Orders.AsNoTracking();
 
-        if (!currentUser.IsAdmin)
+        //if (!currentUser.IsAdmin)
+        //{
+        //    q = q.Where(x => x.MarketUserId == currentUser.UserId);
+        //}
+        //var searchTerm = request.Search?.Trim().ToLower() ?? string.Empty;
+
+        //if (!string.IsNullOrWhiteSpace(request.Search))
+        //{
+        //    q = q.Where(x => x.ReferenceNumber.ToLower().Contains(searchTerm));
+        //}
+
+        //var projectedQuery = q.OrderBy(x => x.OrderedAtUtc)
+        //    .Select(x => new ListOrdersQueryDto
+        //    {
+        //        Id = x.Id,
+        //        ReferenceNumber = x.ReferenceNumber,
+        //        User = new ListOrdersQueryDtoUser
+        //        {
+        //            UserFirstname = x.MarketUser!.Firstname,
+        //            UserLastname = x.MarketUser!.Lastname,
+        //            UserAddress = "Todo",//todo: ticket no 126
+        //            UserCity = "Todo",//todo: ticket no 126
+        //        },
+        //        OrderedAtUtc = x.OrderedAtUtc,
+        //        PaidAtUtc = x.PaidAtUtc,
+        //        Status = x.Status,
+        //        TotalAmount = (decimal)x.TotalAmount,
+        //        Note = x.Note,
+        //    });
+
+        //return await PageResult<ListOrdersQueryDto>.FromQueryableAsync(projectedQuery, request.Paging, ct);
+
+
+        return new PageResult<ListOrdersQueryDto>
         {
-            q = q.Where(x => x.MarketUserId == currentUser.UserId);
-        }
-        var searchTerm = request.Search?.Trim().ToLower() ?? string.Empty;
-
-        if (!string.IsNullOrWhiteSpace(request.Search))
-        {
-            q = q.Where(x => x.ReferenceNumber.ToLower().Contains(searchTerm));
-        }
-
-        var projectedQuery = q.OrderBy(x => x.OrderedAtUtc)
-            .Select(x => new ListOrdersQueryDto
-            {
-                Id = x.Id,
-                ReferenceNumber = x.ReferenceNumber,
-                User = new ListOrdersQueryDtoUser
-                {
-                    UserFirstname = x.MarketUser!.Firstname,
-                    UserLastname = x.MarketUser!.Lastname,
-                    UserAddress = "Todo",//todo: ticket no 126
-                    UserCity = "Todo",//todo: ticket no 126
-                },
-                OrderedAtUtc = x.OrderedAtUtc,
-                PaidAtUtc = x.PaidAtUtc,
-                Status = x.Status,
-                TotalAmount = x.TotalAmount,
-                Note = x.Note,
-            });
-
-        return await PageResult<ListOrdersQueryDto>.FromQueryableAsync(projectedQuery, request.Paging, ct);
+            Items = new List<ListOrdersQueryDto>(),
+            TotalItems = 0,      
+            CurrentPage = 1,     // Ovo je required
+            TotalPages = 0,      // Ovo je required
+            PageSize = 10,       // Ovo je required
+            IncludedTotal = false    // Ovo je required
+        };
     }
 }
